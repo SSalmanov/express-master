4f721b7b26ddf0114fc4c7615bccac87edbaa9a0
SSalmanov <sabirsalmanov555@gmail.com>
2024-05-30 16:47:10 +0400

    dewyis

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express')
+const app = express()
 
 
-const employees = () =>{
-    const data = fs.readFileSync('user.json');
-    return JSON.parse(data);
-}
+const cards = [
+    { id: 1, cardNumber: '1234-5678-9012-3456' },
+    { id: 2, cardNumber: '2345-6789-0123-4567' },
+    { id: 3, cardNumber: '3456-7890-1234-5678' },
+    { id: 4, cardNumber: '4567-8901-2345-6789' },
+    { id: 5, cardNumber: '5678-9012-3456-7890' }
+];
 
 
 
-app.get('/', (req, res) => {
-    res.send("salam");
-});
 
-app.get('/card', (req, res) => {
-    res.send("Bu route kartlar üçün cavabdehdir");
-});
+// app.get('/card/:id', (req, res) => {
+    
+//     const cardid = parseInt(req.params.id);
+//     const card = cards.find(card => card.id === cardid);
+//     if (card) {
+//         res.send(JSON.stringify(card));
+//     } else {
+//         res.send(JSON.stringify({}));
+//     }
 
-app.get('/client', (req, res) => {
-    res.send("Bu marşrut müştərilər üçün cavabdehdir");
-});
+// })
 
-app.get('/employee', (req, res) => {
-    res.send(employees());
-});
+
+
+app.get('/card/:id', (req, res) => {
+    
+    const cardid = parseInt(req.params.id);
+    const card = cards.find(card => card.id === cardid);
+    if (card) {
+        res.send(JSON.stringify(card));
+    } else {
+        res.status(404).send();
+    }
+
+})
+
+
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000/');
-});
+    console.log('http://localhost:3000/card/6')
+})
